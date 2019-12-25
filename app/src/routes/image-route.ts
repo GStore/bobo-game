94df59da60d47dff23deccdd0a7c43019e314b11
Gun Store <ggunstore@gmail.com>
2019-12-25 21:44:50 +0000

    added basic logging

--- app/src/routes/image-route.ts
+++ app/src/routes/image-route.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import * as fs from "fs";
 const imageRoute: express.IRouter = express.Router();
 const imageLocation: string=process.env.BG_IMAGES || "/undefined";
+const loglevel:string | undefined="info";
 
+if(loglevel ==="debug") { 
+  const files = fs.readdirSync(imageLocation);
+  for(let f in files) {
+    console.log(files[f]);
+  }
+}
 imageRoute.use("/images",express.static(imageLocation));
 
 export default imageRoute;
