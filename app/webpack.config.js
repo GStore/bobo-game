86506a6042b37fc3cf1cc0f4becbad4c6e8b5d56
Gun.Store <ggunstore@gmail.com>
2019-12-26 21:22:33 +0000

    WIP: html-webpack-plugin, template parameters

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,13 +1,18 @@
 const nodeexternals = require("webpack-node-externals");
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+const UI = process.env.UI_BUILD || false;
 
 module.exports = (config) => {
   return {
-    entry: path.resolve(__dirname, 'src/index.ts'),
+    entry:  UI ? path.resolve(__dirname, 'src/ui/index.ts') :path.resolve(__dirname, 'src/index.ts'),
 
     output: {
-      filename: 'index.js',
+      filename: UI ? "ui/index.js" : 'index.js',
       path: path.resolve(__dirname, 'dist')
     },
 
@@ -19,11 +24,37 @@ module.exports = (config) => {
       rules: [{ test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }],
     },
 
-    plugins: [
+    plugins: UI ? [
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+          filename: "ui/index.html",
+          template: path.resolve(__dirname, "src/ui/public/index.html")
+        }),
+        new webpack.EnvironmentPlugin({
+          BG_BASE_URL: "'/'"
+        }
+        ),
+        new CopyPlugin(
+          [
+            {
+              from: path.resolve(__dirname, "src/ui/public"),
+              to: path.resolve(__dirname, "dist"),
+              toType: 'dir',
+              ignore: [
+                '.DS_Store',
+                {
+                  glob: 'index.html',
+                  matchBase: false
+                }
+              ]
+            }
+          ]
+        )
+      ] :[
       new ForkTsCheckerWebpackPlugin()
     ],
     externals:[nodeexternals()],
     mode: process.env.NODE_ENV || "development",
-    target: 'node'
+    target: UI ? "web" : 'node'
   }
 };
