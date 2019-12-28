a762dc3e669fbe692600965f50f09f1351a57206
Gun.Store <ggunstore@gmail.com>
2019-12-28 22:50:12 +0000

    WIP: respond to keypress

--- app/ui/src/views/alphabet-cards.vue
+++ app/ui/src/views/alphabet-cards.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
     Alphabet cards
-
+    {{keyPress}}
     <img src="images/apple.png" />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component<AlphabetCards>({
+  mounted:()=> {
+    window.addEventListener('keyup', event => {
+      this.$data.model.keyPress = event.keyCode;
+    });
+  }
+})
+export default class AlphabetCards extends Vue {
+  private model: any = {
+    keyPress: ""
+  }
+}
+
+</script>}
