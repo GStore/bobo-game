7f225b94e32969e4249c041a0470de8996f941a1
Gun.Store <ggunstore@gmail.com>
2020-01-01 15:49:46 +0000

    Added images/manifest route

--- app/server/src/routes/image-route.ts
+++ app/server/src/routes/image-route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as fs from "fs";
 import path from "path";
+import admZip from "adm-zip";
 
 import logger from "../middleware/logger";
 
@@ -13,6 +14,14 @@ if (!fs.existsSync(imageLocation)) {
     logger.log("fatal", "folder not found");
     throw new Error("unable to find folder");
 }
+imageRoute.get("/images/manifest", (req: express.Request, res: express.Response) => {
+    const cardLocation = `${imageLocation}/cards`
+    const files = fs.readdirSync(cardLocation);
+    const zip = new admZip(`${cardLocation}/${files[0]}`);
+    const entries = zip.getEntries();
+    const zipfiles = entries.map(e => e.entryName);
+    res.json(zipfiles);
+});
 imageRoute.get("/images/:letter", (req: express.Request, res: express.Response) => {
     const letter = req.params.letter;
     const files = fs.readdirSync(imageLocation);
